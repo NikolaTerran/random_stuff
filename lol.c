f5af9b86e376e2fe5aa3991f0350084711b5870a
Nikola Terran <nikolaterranthe1@gmail.com>
2019-01-31 11:54:03 -0500

    s

--- lol.c
+++ lol.c
@@ -12,7 +12,7 @@ char line[20];
 int rgb[3];
 
 fd = open("image.ppm", O_CREAT | O_TRUNC | O_WRONLY, 0644);
-snprintf(line, sizeof(line), "P3 500 500 255\n");
+snprintf(line, sizeof(line), "P3 500 500 50\n");
 write (fd, line, strlen(line));
 
 	for(i=0; i<500; i ++){
@@ -20,7 +20,7 @@ write (fd, line, strlen(line));
 			rgb[0] = i % 255;
 			rgb[1] = j % 255;
 			rgb[2] = 255;
-			snprintf(line, sizeof(line), "%d %d %d", rgb[0], rgb[1], rgb[2]);
+			snprintf(line, sizeof(line), "%d %d %d\n", rgb[0], rgb[1], rgb[2]);
 			write(fd, line, strlen(line));
 	}	 
 	}
